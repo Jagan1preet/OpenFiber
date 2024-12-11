22c90f21ceaf72d3d60518cf52cc545b0742bd75
Jagan Preet <jpreet@eagleprojects.it>
2024-12-11 10:42:02 +0100

    Updated code

--- src/test/java/testCases/DataCompare.java
+++ src/test/java/testCases/DataCompare.java
@@ -1,16 +1,23 @@
 package testCases;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import pageEvents.adminPageEvents.AdminProjectEvents;
 import pageEvents.adminPageEvents.DownloadCMTFile;
-
-
 import java.io.IOException;
-
 
 public class DataCompare extends LogIn {
 
     AdminProjectEvents adminProjectsRoutes = new AdminProjectEvents();
+
+
+    @BeforeMethod
+    @Parameters("browser") // Expecting a parameter named 'browser'
+    public void setUp(@Optional("chrome") String browser) {
+        System.out.println("Setting up tests for browser: " + browser);
+    }
 
     // Test case for Downloading CMT file and compare it with old file
     @Test(description = "Verify user able to download and compare CMT file ")
